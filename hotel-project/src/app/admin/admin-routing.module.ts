627becdfe7180370683391f98ea5992c4846c10d
anagheem63 <meganaalrabaa@gmail.com>
2025-04-16 01:00:40 +0300

    add-routinadmin

--- hotel-project/src/app/admin/admin-routing.module.ts
+++ hotel-project/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
+import { AddRoomComponent } from './pages/add-room/add-room.component';
+import { RoomsListComponent } from './pages/rooms-list/rooms-list.component';
+import { CustomerListComponent } from './pages/customer-list/customer-list.component';
+import { ReservationsReviewComponent } from './pages/reservations-review/reservations-review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'rooms', component: RoomsListComponent },
+  { path: 'add-room', component: AddRoomComponent },
+  { path: 'custmouer-list', component: CustomerListComponent},
+  {path: 'reservations-review', component: ReservationsReviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
