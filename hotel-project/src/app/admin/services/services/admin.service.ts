f18e8b969e9208156692f268249472d18f16280c
anagheem63 <meganaalrabaa@gmail.com>
2025-04-17 23:02:49 +0300

    anagheem_ht_project

--- hotel-project/src/app/admin/services/services/admin.service.ts
+++ hotel-project/src/app/admin/services/services/admin.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { Room } from '../../../models/room.model';
+import { Employee } from '../../../models/employee.model';
+import { StorageService } from '../localstroge..service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  private EMPLOYEES_KEY = 'employees'; 
+private readonly ROOMS_KEY = 'rooms';
+
+  constructor(private storageService: StorageService) { }
+
+  addEmployee(employee: Employee): Promise<Employee> {
+    return new Promise((resolve) => {
+      const employees: Employee[] = JSON.parse(localStorage.getItem(this.EMPLOYEES_KEY) || '[]');
+  
+      const maxId = employees.length > 0 ? Math.max(...employees.map(e => Number(e.id))) : 0;
+      employee.id = maxId + 1;
+  
+      employees.push(employee);
+      localStorage.setItem(this.EMPLOYEES_KEY, JSON.stringify(employees));
+      resolve(employee);
+    });
+  }
+  
+
+  getEmployees(): Promise<Employee[]> {
+    return new Promise((resolve) => {
+      const employees = JSON.parse(localStorage.getItem(this.EMPLOYEES_KEY) || '[]');
+      resolve(employees);
+    });
+  }
+    getRooms(): Room[] {
+    return JSON.parse(localStorage.getItem(this.ROOMS_KEY) || '[]');
+  }
+
+   addRoom(room: Room): Promise<Room> {
+    return new Promise(resolve => {
+      const rooms = this.getRooms();
+      room.id = rooms.length + 1;
+
+     
+      room.booked       = room.booked       ?? '';
+      room.bookedStatus = room.bookedStatus ?? false;
+
+      rooms.push(room);
+      localStorage.setItem(this.ROOMS_KEY, JSON.stringify(rooms));
+      resolve(room);
+    }); 
+    
+  }
+
+updateRoom(updated: Room): Promise<void> {
+    return new Promise(resolve => {
+      const rooms = this.getRooms();
+      const idx = rooms.findIndex(r => r.id === updated.id);
+      if (idx > -1) {
+        rooms[idx] = { ...updated };
+        localStorage.setItem(this.ROOMS_KEY, JSON.stringify(rooms));
+      }
+      resolve();
+    });
+  }
 }
