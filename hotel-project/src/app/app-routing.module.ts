96e2a12c5b4ce7f2db04c4436f46ab11c0d8ec2a
anagheem63 <meganaalrabaa@gmail.com>
2025-04-17 23:18:04 +0300

    anagheem_ht_prject

--- hotel-project/src/app/app-routing.module.ts
+++ hotel-project/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'auth',
+     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'employee', loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule) },
+  {path:'room', loadChildren: () => import('./rooms/rooms.module').then(m => m.RoomsModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
