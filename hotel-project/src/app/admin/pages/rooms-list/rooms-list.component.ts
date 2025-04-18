2db38ae8a43630b8040e58348c4bb7040d9f7703
anagheem63 <meganaalrabaa@gmail.com>
2025-04-18 15:40:45 +0300

    anagheem_ht_project

--- hotel-project/src/app/admin/pages/rooms-list/rooms-list.component.ts
+++ hotel-project/src/app/admin/pages/rooms-list/rooms-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/services/admin.service';
+import { Room } from '../../../models/room.model';
 
 @Component({
   selector: 'app-rooms-list',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.scss'
 })
-export class RoomsListComponent {
+export class RoomsListComponent { 
+     rooms: Room[] = [];
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.loadRooms();
+  }
+
+  private loadRooms(): void {
+    this.rooms = this.adminService.getRooms();
+  }
+
+  toggleBooking(room: Room): void {
+    room.bookedStatus = !room.bookedStatus;
+    this.adminService.updateRoom(room).then(() => {
+      this.loadRooms();
+    });
+  }
+
 
 }
