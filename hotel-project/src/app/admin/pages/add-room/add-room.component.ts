640ac47a26306c9cc72dedcbfe95bb411a980921
anagheem63 <meganaalrabaa@gmail.com>
2025-04-18 15:38:09 +0300

    anagheem_ht_project

--- hotel-project/src/app/admin/pages/add-room/add-room.component.ts
+++ hotel-project/src/app/admin/pages/add-room/add-room.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Room } from '../../../models/room.model';
+import { AdminService } from '../../services/services/admin.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-room',
@@ -6,6 +9,42 @@ import { Component } from '@angular/core';
   templateUrl: './add-room.component.html',
   styleUrl: './add-room.component.scss'
 })
-export class AddRoomComponent {
+export class AddRoomComponent { 
+  room: Room = {
+    booked: '',            
+    id: 0,                  
+    title: '',
+    roomType: 'room',       
+    floor: '',             
+    building: '',
+    details: '',
+    bookedStatus: false    
+  };
+
+  constructor(private adminService: AdminService , private router: Router) { }
+
+  onSubmit(): void {
+
+    this.room.booked       = this.room.booked       ?? '';
+    this.room.bookedStatus = this.room.bookedStatus ?? false;
+
+    this.adminService.addRoom(this.room).then(() => {
+      alert('Room added successfully!');
+    
+      this.room = {
+        booked: '',
+        id: 0,
+        title: '',
+        roomType: 'room',
+        floor: '',
+        building: '',
+        details: '',
+        bookedStatus: false
+      };
+    });
+        this.adminService.addRoom(this.room).then(() => {
+      this.router.navigate(['/admin/rooms']);
+  });
+  }
 
 }
