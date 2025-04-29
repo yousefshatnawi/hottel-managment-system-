9fa8ae3eb00c6c35b85e5380eff95f8633d9bb17
anagheem63 <meganaalrabaa@gmail.com>
2025-04-29 10:33:33 +0300

    editdatabase

--- hotel-project/src/app/shared/dataBase/employee.ts
+++ hotel-project/src/app/shared/dataBase/employee.ts
@@ -7,4 +7,37 @@ export const employees: Employee[] = [
     { id: 4, name: 'Sarah Jones' ,role:'employee'    ,    email: 'John.Doe@gmail.com',    },
     { id: 5, name: 'Michael Brown',role:'employee',        email: 'yousef25@gmail.com'
     },
+     { id: 6, name: 'Michael Brown',role:'employee',        email: 'yousef25@gmail.com'
+    },
+     { id: 7, name: 'Anagheem ',role:'employee',        email: 'yousef15@gmail.com'
+    },
+     { id: 8, name: 'Duaa',role:'employee',        email: 'yousef35@gmail.com'
+    },
+     { id: 9, name: 'Remaa',role:'employee',        email: 'yousef45@gmail.com'
+    },
+     { id: 10, name: 'Yousef',role:'employee',        email: 'yousef55@gmail.com'
+    },
+     { id: 11, name: 'Sarha Jones',role:'employee',        email: 'yousef65@gmail.com'
+    },
+       { id: 12, name: 'reema', role:'employee' ,        email: 'reema1@gmail.com',     },
+     { id: 13, name: 'David Lee',role:'employee',        email: 'David1.Lee@gmail.com',      },
+    { id: 14, name: 'Sarah Jones' ,role:'employee'    ,    email: 'John1.Doe@gmail.com',    },
+    { id: 15, name: 'Michael Brown',role:'employee',        email: 'yousef6@gmail.com'
+    },
+           { id: 16, name: 'reema', role:'employee' ,        email: 'reema3@gmail.com',     },
+    { id: 17, name: 'John Doe' ,role:'employee',        email: 'John.Doe@gmail.com',     },
+     { id: 18, name: 'David Lee',role:'employee',        email: 'David.Lee@gmail.com',      },
+    { id: 19, name: 'Sarah Jones' ,role:'employee'    ,    email: 'John.Doe2@gmail.com',    },
+    { id: 20, name: 'Michael Brown',role:'employee',        email: 'yousef9@gmail.com'
+    },
+         { id: 21, name: 'Anagheem ',role:'employee',        email: 'yousef25@gmail.com'
+    },
+     { id: 22, name: 'Duaa',role:'employee',        email: 'yousef10@gmail.com'
+    },
+     { id: 23, name: 'Remaa',role:'employee',        email: 'yousef11@gmail.com'
+    },
+     { id: 24, name: 'Yousef',role:'employee',        email: 'yousef12@gmail.com'
+    },
+     { id: 25, name: 'Sarha Jones',role:'employee',        email: 'yousef14@gmail.com'
+    },
 ];
