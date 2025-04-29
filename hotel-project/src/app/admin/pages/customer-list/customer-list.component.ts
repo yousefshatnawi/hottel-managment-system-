08e7018963884862af0435be7e8940ca7623310b
reema995 <reemabsh95@gmail.com>
2025-04-29 11:48:54 +0300

    29.4

--- hotel-project/src/app/admin/pages/customer-list/customer-list.component.ts
+++ hotel-project/src/app/admin/pages/customer-list/customer-list.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { AdminService } from '../../services/services/admin.service';
 import { Customer } from '../../../models/customer.model';
 import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-customer-list',
@@ -9,10 +11,20 @@ import { Router } from '@angular/router';
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss'
 })
-export class CustomerListComponent { 
+export class CustomerListComponent implements AfterViewInit { 
    customers: Customer[] = [];
+   displayedColumns: string[] = ['name', 'phone', 'email', 'address'];
+   dataSource = new MatTableDataSource<Customer>(this.customers);
+ 
 
-  constructor(private adminService: AdminService ,private router:Router) {}
+
+   @ViewChild(MatPaginator) paginator!: MatPaginator;
+ constructor(private adminService: AdminService ,private router:Router) {}
+   ngAfterViewInit(): void {
+     this.dataSource.paginator = this.paginator;
+   }
+ 
+  
 
   ngOnInit(): void {
     this.loadCustomers();
@@ -27,8 +39,12 @@ export class CustomerListComponent {
 
   loadCustomers(): void {
     this.customers = this.adminService.getCustomers();
+    this.customers = this.dataSource.data = this.customers;
   } 
 
-
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
 
 }
