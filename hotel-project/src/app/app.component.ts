a68d5a9eb75eae415b9688ee4b5fc85fc2afeb6d
reema995 <reemabsh95@gmail.com>
2025-05-06 23:14:29 +0300

    6.5

--- hotel-project/src/app/app.component.ts
+++ hotel-project/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from './services/language.service';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,38 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  ngOnInit(): void {
-    
-    
-  }
+export class AppComponent implements OnInit {
+ 
+
+    constructor(
+      private languageService: LanguageService,
+      private translate: TranslateService
+    ) {}
   
-}
+    ngOnInit() {
+      // نحمل اللغة المحفوظة أو نستخدم الإنجليزية كافتراضي
+      const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+      
+      // نضبط اللغة في السيرفيس و ngx-translate
+      this.languageService.changeLanguage(savedLanguage);
+      this.translate.use(savedLanguage);
+  
+      // نضبط اتجاه الصفحة حسب اللغة
+      this.setDirection(savedLanguage);
+  
+      // في حال تغيرت اللغة لاحقًا، نحدث ngx-translate تلقائيًا
+      this.languageService.currentLanguage.subscribe(lang => {
+        this.translate.use(lang);
+        this.setDirection(lang);
+      });
+    }
+  
+    // دالة لتحديد اتجاه الصفحة
+    setDirection(language: string) {
+      if (language === 'ar') {
+        document.documentElement.setAttribute('dir', 'rtl');
+      } else {
+        document.documentElement.setAttribute('dir', 'ltr');
+      }
+    }
+  }
